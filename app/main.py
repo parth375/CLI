77e089691fcb8ece9278707d49a2896b7b2bc037
parth375 <parthchauhan.375@gmail.com>
2026-03-22 20:10:44 +0530

    cli

---
+++ app/main.py
@@ -0,0 +1,20 @@
+#CLI
+
+# Read User commands - starting with fod
+def add():
+
+def create():
+
+def remove():
+
+def delete():
+
+def display():
+
+
+def main():
+    print("Enter Mail")
+    print("Welcome to THC what we are going to do today.")
+    user_input=input()
+
+
